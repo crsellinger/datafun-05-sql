dc0481cc1e028dafd8fe3403bc4b271eabdb3ada
Caleb Sellinger <calebsell24@gmail.com>
2025-02-10 17:19:54 -0600

    Create database

--- sql_create/02_create_tables.sql
+++ sql_create/02_create_tables.sql
@@ -1,12 +1,21 @@
+-- Create the authors table 
+-- Note that the author table has no foreign keys, so it is a standalone table
+
 CREATE TABLE authors (
     author_id TEXT PRIMARY KEY,
-    first TEXT NOT NULL,
-    last TEXT NOT NULL
+    first TEXT,
+    last TEXT,
 );
+
+-- Create the books table
+-- Note that the books table has a foreign key to the authors table
+-- This means that the books table is dependent on the authors table
+-- Be sure to create the standalone authors table BEFORE creating the books table.
 
 CREATE TABLE books (
     book_id TEXT PRIMARY KEY,
-    title TEXT NOT NULL,
-    year_published TEXT NOT NULL,
-    author_id TEXT FOREIGN KEY
-    );
+    title TEXT,
+    year_published INTEGER,
+    author_id TEXT,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
